050471a472b29cab9190155d60c430c23faaf373
Stephen Gitau <stephenpepe.sm@gmail.com>
2024-01-24 13:46:55 -0800

    use union to sum mixed list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Sum mixed list use union """
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Func doc"""
+    return sum(mxd_lst)
